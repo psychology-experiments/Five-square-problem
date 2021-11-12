03262bd23403cbb25fa261d7fa68d85af35046e0
Makarov Igor <reoge@mail.ru>
2021-11-12 14:35:01 +0300

    Refactored grid class

--- js/katona/presenter/logic/grid.js
+++ js/katona/presenter/logic/grid.js
@@ -8,38 +8,31 @@ class GridElement {
 
 
 class Grid {
-    constructor({ startPoint, gridSize, gridUnitLength, gridUnitWidth }) {
+    constructor({ startPoint, gridSquares, gridUnitLength, gridUnitWidth }) {
         this.startPoint = startPoint;
-        this.gridSize = gridSize;
-        this.gridUnitLength = gridUnitLength;
-        this.gridUnitWidth = gridUnitWidth;
-        this.step = this.gridUnitLength + this.gridUnitWidth;
+        this.gridSquares = gridSquares;
+
+        this.evenRowPositionShift = gridUnitWidth / 2 + gridUnitLength / 2;
+        this.step = gridUnitLength + gridUnitWidth;
 
         this.gridElements = [];
         this._createGrid();
     }
 
     _createGrid() {
-        const [startX, startY] = this.startPoint;
-        const startEven = [startX + this.gridUnitLength / 2, startY - this.gridUnitWidth / 2];
-        const startOdd = [startX - this.gridUnitWidth / 2, startY - this.gridUnitWidth];
-
-        const gridRows = this.gridSize * 2 + 1;
+        const gridRows = this.gridSquares * 2 + 1;
         for (let rowIndex = 0; rowIndex < gridRows; rowIndex++) {
-            const startPosition = rowIndex % 2 === 0 ? startEven : startOdd;
-            const rowElements = this._createRow(rowIndex, startPosition);
+            const rowElements = this._createRow(rowIndex);
             this.gridElements.push.apply(this.gridElements, rowElements);
         }
     }
 
-    _createRow(rowIndex, firstRowPosition) {
+    _createRow(rowIndex) {
         const isOdd = rowIndex % 2 === 1;
-        const columns = this.gridSize + isOdd;
+        const columns = this.gridSquares + isOdd;
 
-        let x = firstRowPosition[0];
-        const y = firstRowPosition[1] -
-            this.gridUnitLength * rowIndex / 2 -
-            this.gridUnitWidth * (rowIndex - isOdd) / 2;
+        const x = this.startPoint[0] + this.evenRowPositionShift * !isOdd;
+        const y = this.startPoint[1] - this.step * rowIndex / 2;
         const elementOrientation = isOdd ? 0 : 90;
 
         const rowElements = [];
